701ef93331a00a29381e313b9883a0f218b2f69a
Max Glassie <msglassie@gmail.com>
2017-05-11 11:16:44 -0600

    completes setup

---
+++ test/food-test.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+var Food = require('../lib/food');
+
+describe('Food', function() {
+  context('can create a new food', function() {
+    // Your tests here...
+  });
+});
